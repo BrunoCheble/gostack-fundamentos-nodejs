71d5a61a262b12c95f4809532f6e11ca75b34578
brunocheble <contato.cheble@gmail.com>
2020-04-17 18:53:05 +0100

    Development Transaction List; Development Transaction Create; Development Transaction Create Validation;

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,6 +1,12 @@
 import Transaction from '../models/Transaction';
 
-interface Balance {
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface Balace {
   income: number;
   outcome: number;
   total: number;
@@ -9,20 +15,43 @@ interface Balance {
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balace;
+
   constructor() {
     this.transactions = [];
+    this.balance = { income: 0, outcome: 0, total: 0 };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
-  public getBalance(): Balance {
-    // TODO
+  public getBalance(): Balace {
+    const income = this.transactions.reduce(
+      (accumulator, transaction) =>
+        transaction.type === 'income'
+          ? accumulator + transaction.value
+          : accumulator,
+      0,
+    );
+
+    const outcome = this.transactions.reduce(
+      (accumulator, transaction) =>
+        transaction.type === 'outcome'
+          ? accumulator + transaction.value
+          : accumulator,
+      0,
+    );
+
+    const total = income - outcome;
+    this.balance = { income, outcome, total };
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
